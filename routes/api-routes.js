71d7390b408737700a9b4273c32a3d19c5bd58c6
toshyamsundar <toshyamsundar@yahoo.com>
2019-06-18 18:45:48 -0400

    Added routes folder for api and html routes Moved routes to corresponding js files Added path package for html routes

---
+++ routes/api-routes.js
@@ -0,0 +1,23 @@
+let db = require("./models");
+
+app.get("/", (req, res) => {
+  response.send("Hello Wold");
+});
+
+app.get("/scrape", (req, res) => {
+  axios
+    .get("https://www.nytimes.com/")
+    .then(results => {
+      // console.log(results.data);
+      // res.send(results.data);
+      let $ = cheerio.load(results.data);
+
+      $("article").each((i, element) => {
+        console.log(element);
+      });
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(404).json(error);
+    });
+});
---
+++ routes/html-routes.js
@@ -0,0 +1,7 @@
+let path = require("path");
+
+module.exports = app => {
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+};
