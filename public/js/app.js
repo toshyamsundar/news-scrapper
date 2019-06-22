19e53d64fdb822540e9d72fd0e660a92f05d5d2c
toshyamsundar <toshyamsundar@yahoo.com>
2019-06-22 00:09:06 -0400

    Reduced the bg image height in css Added client js to render articles returned Modified html container for articles

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,30 @@
 $(document).on("click", "#scrapeArticles", event => {
   $.get("/scrape")
     .then(results => {
-      console.log(results);
+      console.log(results.dbArticles[0]);
+      renderArticles(results);
     })
     .catch(error => {
       console.log(error);
     });
 });
+
+let renderArticles = data => {
+  data.dbArticles.forEach(article => {
+    let divCard = $("<div>").addClass("card my-1 bg-light");
+    let divCardBody = $("<div>").addClass("card-body d-flex justify-content-between");
+    let h4Elem = $("<h4>");
+    let aTag = $("<a>")
+      .attr("href", article.url)
+      .attr("target", "_blank")
+      .text(article.headline);
+    let btnElem = $("<button>")
+      .addClass("btn btn-success")
+      .text("Save");
+
+    $(h4Elem).append(aTag);
+    $(divCardBody).append(h4Elem, btnElem);
+    $(divCard).append(divCardBody);
+    $("#news-articles").append(divCard);
+  });
+};
