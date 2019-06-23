993ddba4194c3d0dcf166612cc32153dd118bcb7
toshyamsundar <toshyamsundar@yahoo.com>
2019-06-22 23:30:00 -0400

    Added js to call getSavedArticles endpoint

--- public/js/app.js
+++ public/js/app.js
@@ -2,14 +2,14 @@ $(document).on("click", "#scrapeArticles", event => {
   $.get("/api/scrape")
     .then(results => {
       console.log(results.dbArticles[0]);
-      renderArticles(results);
+      renderScrapedArticles(results);
     })
     .catch(error => {
       console.log(error);
     });
 });
 
-let renderArticles = data => {
+let renderScrapedArticles = data => {
   $("#news-articles").empty();
   data.dbArticles.forEach(article => {
     let divCard = $("<div>")
@@ -45,3 +45,38 @@ $(document).on("click", ".save-article", function(event) {
       .addClass("btn-danger");
   });
 });
+
+$(document).on("click", "#savedArticles", function(event) {
+  event.preventDefault();
+
+  $.get("/api/getSavedArticles")
+    .then(results => {
+      console.log(results.dbArticles[0]);
+      renderSavedArticles(results);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+let renderSavedArticles = data => {
+  data.dbArticles.forEach(article => {
+    let divCard = $("<div>")
+      .addClass("card my-1 bg-light")
+      .attr("data-id", article._id);
+    let divCardBody = $("<div>").addClass("card-body d-flex justify-content-between");
+    let h4Elem = $("<h4>");
+    let aTag = $("<a>")
+      .attr("href", article.url)
+      .attr("target", "_blank")
+      .text(article.headline);
+    let btnElem = $("<button>")
+      .addClass("btn btn-success save-article")
+      .text("Save");
+
+    $(h4Elem).append(aTag);
+    $(divCardBody).append(h4Elem, btnElem);
+    $(divCard).append(divCardBody);
+    $("#saved-articles").append(divCard);
+  });
+};
